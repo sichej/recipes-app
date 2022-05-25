3b6644c53f535e2d2b639558f907dbd762250284
Edosicca <47154408+sichej@users.noreply.github.com>
2022-05-25 14:55:51 +0200

    Update

--- src/pages/addRecipe.js
+++ src/pages/addRecipe.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import spaghetti from '../img/spaghetti.png';
   
 const AddRecipe = () => {
 
--- src/pages/recipe.js
+++ src/pages/recipe.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
-import spaghetti from '../img/spaghetti.png';
   
 const Recipe = () => {
   const { id } = useParams();
